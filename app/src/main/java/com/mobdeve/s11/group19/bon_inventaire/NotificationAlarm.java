2d4ff987a880ba2b73e327c1e5246b65278141f6
sophia <sophia_eguaras@dlsu.edu.ph>
2021-09-06 07:41:52 +0800

    V7.1
    - Added a notification receiver that extends BroadcastReceiver for date related notifications

---
+++ app/src/main/java/com/mobdeve/s11/group19/bon_inventaire/NotificationAlarm.java
@@ -0,0 +1,29 @@
+package com.mobdeve.s11.group19.bon_inventaire;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
+
+import java.util.Random;
+
+public class NotificationAlarm extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        String title = intent.getStringExtra(Keys.KEY_TITLE.name());
+        String body = intent.getStringExtra(Keys.KEY_MSG.name());
+        String channel_id = intent.getStringExtra(Keys.KEY_CHANNEL_ID.name());
+
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(context, channel_id);
+        builder.setAutoCancel(true)
+                .setSmallIcon(R.drawable.app_name_logo)
+                .setContentTitle(title)
+                .setContentText(body);
+
+        NotificationManagerCompat manager = NotificationManagerCompat.from(context);
+        manager.notify(new Random().nextInt(), builder.build());
+    }
+}
