466c4b1dfb19ee311fc48175a764f27c3c51b5f0
sophia <sophia_eguaras@dlsu.edu.ph>
2021-09-07 16:32:04 +0800

    V8.3
    - Added repeating notification for added items that are zero (0) in stock (Follows a 3-day interval per notification that the stock is not changed from 0)
    - Added a notification for expiration notifications (Follows 1-day, 3-day, and 7-day notifications)
    - Added method to add the item's owner's name in the notification (user that owns the item for the notification)
    - Removed checkDate method
    - Made notifications clickable

--- app/src/main/java/com/mobdeve/s11/group19/bon_inventaire/AddItemActivity.java
+++ app/src/main/java/com/mobdeve/s11/group19/bon_inventaire/AddItemActivity.java
@@ -141,8 +141,6 @@ public class AddItemActivity extends AppCompatActivity {
     }
 
     private void initSave() {
-        String initDate = etExpireDate.getText().toString();
-
         this.ibSave = findViewById(R.id.ib_add_item_save);
         this.ibSave.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -162,7 +160,7 @@ public class AddItemActivity extends AppCompatActivity {
                         list = "Unlisted";
                     Item item = new Item(name, list, note, Integer.parseInt(numStocks),expireDate, id);
 //                    retrieveItem(item);
-                    retrieveItem(item, initDate);
+                    retrieveItem(item);
                 }
                 else
                     Toast.makeText(getApplicationContext(), "Adding Item Failed", Toast.LENGTH_SHORT).show();
@@ -204,7 +202,7 @@ public class AddItemActivity extends AppCompatActivity {
     }
 
 //    public void retrieveItem(Item item) {
-    public void retrieveItem(Item item, String initDate) {
+    public void retrieveItem(Item item) {
         Toast.makeText(getApplicationContext(), "Adding item to the database...", Toast.LENGTH_SHORT).show();
         pbAddItem.setVisibility(View.VISIBLE);
         mDatabase.getReference(Collections.users.name())
@@ -219,12 +217,12 @@ public class AddItemActivity extends AppCompatActivity {
                             allItem = new ArrayList<Item>();
                             allItem.add(0,item);
 //                            storeItem(allItem);
-                            storeItem(allItem, initDate);
+                            storeItem(allItem);
                         } else {
                             item.setItemID(allItem.size());
                             allItem.add(0,item);
 //                            storeItem(allItem);
-                            storeItem(allItem, initDate);
+                            storeItem(allItem);
                         }
                     }
                     @Override
@@ -248,7 +246,7 @@ public class AddItemActivity extends AppCompatActivity {
 //    }
 
 //    private void storeItem(ArrayList<Item> allItem) {
-    private void storeItem(ArrayList<Item> allItem, String initDate) {
+    private void storeItem(ArrayList<Item> allItem) {
         mDatabase.getReference(Collections.users.name())
                 .child(mAuth.getCurrentUser().getUid()).child(Collections.items.name())
                 .setValue(allItem)
@@ -266,8 +264,9 @@ public class AddItemActivity extends AppCompatActivity {
                             intent.putExtra(Keys.KEY_NOTE.name(), allItem.get(0).getItemNote());
                             intent.putExtra(Keys.KEY_ITEM_ID.name(), allItem.get(0).getItemID());
 
-                            checkDate(allItem.get(0).getItemID(), allItem.get(0).getItemExpireDate().toString(),
-                                initDate);
+                            getUserName(allItem.get(0).getItemID(), allItem.get(0).getItemExpireDate().toString(),
+                                    allItem.get(0).getItemName(), allItem.get(0).getItemNumStocks());
+
                             //TODO
                             Toast.makeText(AddItemActivity.this, "DONE", Toast.LENGTH_SHORT).show();
 
@@ -290,6 +289,25 @@ public class AddItemActivity extends AppCompatActivity {
         });
     }
 
+    private void getUserName(int itemId, String expDate, String itemName, int numStocks) {
+        mDatabase.getReference(Collections.users.name())
+                .child(mAuth.getCurrentUser().getUid()).child(Collections.name.name())
+                .addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot snapshot) {
+                        String name = snapshot.getValue().toString();
+                        initNotifStockRepeat("Bonjour, " + name + "! " + itemName + " is out of stock.",
+                                numStocks, itemId);
+                        initNotifExp("Bonjour, " + name + "! " + itemName, itemId, expDate);
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError error) {
+                        Toast.makeText(getApplicationContext(), "Can't retrieve data", Toast.LENGTH_SHORT).show();
+                    }
+                });
+    }
+
     private void createNotifChannel () {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
             NotificationChannel channel = new NotificationChannel(CHANNEL_ID, CHANNEL_NAME, NotificationManager.IMPORTANCE_DEFAULT);
@@ -309,15 +327,39 @@ public class AddItemActivity extends AppCompatActivity {
         return expAlarm;
     }
 
-    private void checkDate (int itemId, String expDate, String oldDate) {
-        String oldExpDate = oldDate;
-        String newExpDate = expDate;
+    private void initNotifStockRepeat (String body, int numStocks, int itemId) {
+        if (numStocks == 0) {
+            String reqCodeRepeat = Integer.toString(itemId) + "999";
+            AlarmManager alarmManager = (AlarmManager) getSystemService(ALARM_SERVICE);
 
-        if (!(oldExpDate.equals(newExpDate)))
-            initNotifExp("Item expiring soon!", etName.getText().toString() + " will expire in ", itemId, newExpDate);
+            Intent intentRedirect = new Intent(this, HomeActivity.class);
+            intentRedirect.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+            PendingIntent pendingRedirect = PendingIntent.getActivity(this, Integer.parseInt(reqCodeRepeat), intentRedirect, 0);
+
+            Intent intent = new Intent(AddItemActivity.this, NotificationAlarm.class);
+            intent.putExtra(Keys.KEY_TITLE.name(), "Out of stock!");
+            intent.putExtra(Keys.KEY_MSG.name(), body);
+            intent.putExtra(Keys.KEY_CHANNEL_ID.name(), CHANNEL_ID);
+            intent.putExtra(Keys.KEY_REDIRECT_INTENT.name(), pendingRedirect);
+
+            PendingIntent pendInt0d = PendingIntent.getBroadcast(AddItemActivity.this,
+                    Integer.parseInt(reqCodeRepeat), intent, 0);
+
+            alarmManager.setInexactRepeating(AlarmManager.RTC_WAKEUP, 1000 * 30,
+            1000 * 30, pendInt0d);
+
+//        alarmManager.setInexactRepeating(AlarmManager.RTC_WAKEUP, 1000 * 5,
+//                MILISECOND_IN_24HRS * 3, pendInt0d);
+        }
     }
 
-    private void initNotifExp (String title, String body, int itemId, String expDate) {
+    private void initNotifExp (String body, int itemId, String expDate) {
+        String expiredMsg = " has expired";
+        String expireSoonMsg = " will expire in ";
+
+        String expireSoonTitle = "Item expiring soon!";
+        String expiredTitle = "Item expired";
+
         createNotifChannel();
 
         Date expDateInput = null;
@@ -335,39 +377,71 @@ public class AddItemActivity extends AppCompatActivity {
         }
         long timeInMS = timeNow - temp;
         expDateMS += timeInMS;
+        long timeAlarm;
 //        Toast.makeText(AddItemActivity.this, "NOW: " + timeNow, Toast.LENGTH_SHORT).show();
-        long expDate1Day = expDateMS - MILISECOND_IN_24HRS;
-        long timeAlarm = getExpiryDateInMs(expDate1Day, timeNow);
 //        Toast.makeText(AddItemActivity.this, "1Day: " + expDate1Day, Toast.LENGTH_SHORT).show();
 //        Toast.makeText(AddItemActivity.this, "ALARM: " + timeAlarm, Toast.LENGTH_SHORT).show();
+        String reqCode0d = Integer.toString(itemId) + "0";
         String reqCode1d = Integer.toString(itemId) + "1";
         String reqCode3d = Integer.toString(itemId) + "3";
         String reqCode7d = Integer.toString(itemId) + "7";
 
 //        Log.d("EXPMS", "Expiry date: " + expDateMS);
 //        Log.d("EXPMS", "Minus 7 days: " + (expDateMS - (MILISECOND_IN_24HRS * 7)));
-//        Log.d("EXPMS", "Time now: " + timeNow);
+//        Log.d("EXPMS", "Time now: " + timeNow)
 
         AlarmManager alarmManager = (AlarmManager) getSystemService(ALARM_SERVICE);
 
-        Intent intent1d = new Intent(AddItemActivity.this, NotificationAlarm.class);
-        intent1d.putExtra(Keys.KEY_TITLE.name(), title);
-        intent1d.putExtra(Keys.KEY_MSG.name(), body + "one (1) day");
-        intent1d.putExtra(Keys.KEY_CHANNEL_ID.name(), CHANNEL_ID);
+        if (expDateMS == timeNow) {
+            Intent intentRedirect0d = new Intent(this, HomeActivity.class);
+            intentRedirect0d.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+            PendingIntent pendingRedirect0d = PendingIntent.getActivity(this, Integer.parseInt(reqCode0d), intentRedirect0d, 0);
 
-        PendingIntent pendInt1d = PendingIntent.getBroadcast(AddItemActivity.this,
-                Integer.parseInt(reqCode1d), intent1d, 0);
+            Intent intent0d = new Intent(AddItemActivity.this, NotificationAlarm.class);
+            intent0d.putExtra(Keys.KEY_TITLE.name(), expiredTitle);
+            intent0d.putExtra(Keys.KEY_MSG.name(), body + expiredMsg);
+            intent0d.putExtra(Keys.KEY_CHANNEL_ID.name(), CHANNEL_ID);
+            intent0d.putExtra(Keys.KEY_REDIRECT_INTENT.name(), pendingRedirect0d);
 
-        alarmManager.set(AlarmManager.RTC_WAKEUP, timeAlarm, pendInt1d);
+            PendingIntent pendInt0d = PendingIntent.getBroadcast(AddItemActivity.this,
+                    Integer.parseInt(reqCode0d), intent0d, 0);
+
+            alarmManager.set(AlarmManager.RTC_WAKEUP, timeNow, pendInt0d);
+        }
+
+        if (expDateMS - MILISECOND_IN_24HRS >= timeNow) {
+            long expDate1Day = expDateMS - MILISECOND_IN_24HRS;
+            timeAlarm = getExpiryDateInMs(expDate1Day, timeNow);
+
+            Intent intentRedirect1d = new Intent(this, HomeActivity.class);
+            intentRedirect1d.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+            PendingIntent pendingRedirect1d = PendingIntent.getActivity(this, Integer.parseInt(reqCode1d), intentRedirect1d, 0);
+
+            Intent intent1d = new Intent(AddItemActivity.this, NotificationAlarm.class);
+            intent1d.putExtra(Keys.KEY_TITLE.name(), expireSoonTitle);
+            intent1d.putExtra(Keys.KEY_MSG.name(), body + expireSoonMsg + "one (1) day");
+            intent1d.putExtra(Keys.KEY_CHANNEL_ID.name(), CHANNEL_ID);
+            intent1d.putExtra(Keys.KEY_REDIRECT_INTENT.name(), pendingRedirect1d);
+
+            PendingIntent pendInt1d = PendingIntent.getBroadcast(AddItemActivity.this,
+                    Integer.parseInt(reqCode1d), intent1d, 0);
+
+            alarmManager.set(AlarmManager.RTC_WAKEUP, timeAlarm, pendInt1d);
+        }
 
         if (expDateMS - (MILISECOND_IN_24HRS * 3) >= timeNow) {
             long expDate3Days = expDateMS - (MILISECOND_IN_24HRS * 3);
             timeAlarm = getExpiryDateInMs(expDate3Days, timeNow);
 
+            Intent intentRedirect3d = new Intent(this, HomeActivity.class);
+            intentRedirect3d.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+            PendingIntent pendingRedirect3d = PendingIntent.getActivity(this, Integer.parseInt(reqCode3d), intentRedirect3d, 0);
+
             Intent intent3d = new Intent(AddItemActivity.this, NotificationAlarm.class);
-            intent3d.putExtra(Keys.KEY_TITLE.name(), title);
-            intent3d.putExtra(Keys.KEY_MSG.name(), body + "three (3) days");
+            intent3d.putExtra(Keys.KEY_TITLE.name(), expireSoonTitle);
+            intent3d.putExtra(Keys.KEY_MSG.name(), body + expireSoonMsg + "three (3) days");
             intent3d.putExtra(Keys.KEY_CHANNEL_ID.name(), CHANNEL_ID);
+            intent3d.putExtra(Keys.KEY_REDIRECT_INTENT.name(), pendingRedirect3d);
 
             PendingIntent pendInt3d = PendingIntent.getBroadcast(AddItemActivity.this,
                     Integer.parseInt(reqCode3d), intent3d, 0);
@@ -379,10 +453,15 @@ public class AddItemActivity extends AppCompatActivity {
             long expDate7Days = expDateMS - (MILISECOND_IN_24HRS * 7);
             timeAlarm = getExpiryDateInMs(expDate7Days, timeNow);
 
+            Intent intentRedirect7d = new Intent(this, HomeActivity.class);
+            intentRedirect7d.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+            PendingIntent pendingRedirect7d = PendingIntent.getActivity(this, Integer.parseInt(reqCode7d), intentRedirect7d, 0);
+
             Intent intent7d = new Intent(AddItemActivity.this, NotificationAlarm.class);
-            intent7d.putExtra(Keys.KEY_TITLE.name(), title);
-            intent7d.putExtra(Keys.KEY_MSG.name(), body + "seven (7) days");
+            intent7d.putExtra(Keys.KEY_TITLE.name(), expireSoonTitle);
+            intent7d.putExtra(Keys.KEY_MSG.name(), body + expireSoonMsg + "seven (7) days");
             intent7d.putExtra(Keys.KEY_CHANNEL_ID.name(), CHANNEL_ID);
+            intent7d.putExtra(Keys.KEY_REDIRECT_INTENT.name(), pendingRedirect7d);
 
             PendingIntent pendInt7d = PendingIntent.getBroadcast(AddItemActivity.this,
                     Integer.parseInt(reqCode7d), intent7d, 0);
